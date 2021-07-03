87abe9b837e86426f89638b39846d8fe1621cb60
WahidHoquee <wahidhoquee@gmail.com>
2021-07-03 15:52:02 +0600

    Fixed the Semester Info

--- components/admitCardGenerator.js
+++ components/admitCardGenerator.js
@@ -38,7 +38,7 @@ const AdmitCardGenerator = ({studentData}) => {
 
     // Setting up Exam Session
     doc.setFontSize(35)
-    doc.text(82, 100, 'Admit Card - Final Exam')
+    doc.text(82, 100, 'Admit Card - Mid Exam')
 
     //Setting Line
     doc.setDrawColor(44, 45, 146)
@@ -65,7 +65,7 @@ const AdmitCardGenerator = ({studentData}) => {
 
     doc.text(50, 270, 'Semester')
     doc.text(135, 270, ':')
-    doc.text(150, 270, 'Spring - 2021')
+    doc.text(150, 270, 'Summer - 2021')
   
 
     doc.setDrawColor(44, 45, 146)
